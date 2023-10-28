87c0e841f6fc81ee484c5e8248407a91ba9f5fd6
mcliquid <info@mcliquid.de>
2023-10-28 17:20:45 +0200

    re-used existing strings

--- app/src/main/java/de/westnordost/streetcomplete/quests/street_cabinet/AddStreetCabinetTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/street_cabinet/AddStreetCabinetTypeForm.kt
@@ -17,17 +17,17 @@ import de.westnordost.streetcomplete.quests.street_cabinet.StreetCabinetType.WAT
 import de.westnordost.streetcomplete.quests.street_cabinet.StreetCabinetType.SEWERAGE
 class AddStreetCabinetTypeForm : AListQuestForm<StreetCabinetType>() {
     override val items = listOf(
-        TextItem(POWER, R.string.quest_street_cabinet_power),
-        TextItem(TELECOM, R.string.quest_street_cabinet_telecom),
+        TextItem(POWER, R.string.quest_utility_power),
+        TextItem(TELECOM, R.string.quest_utility_telecom),
         TextItem(POSTAL_SERVICE, R.string.quest_street_cabinet_postal_service),
         TextItem(TRAFFIC_CONTROL, R.string.quest_street_cabinet_traffic_control),
         TextItem(TRAFFIC_MONITORING, R.string.quest_street_cabinet_traffic_monitoring),
         TextItem(TRANSPORT_MANAGEMENT, R.string.quest_street_cabinet_transport_management),
         TextItem(WASTE, R.string.quest_street_cabinet_waste),
         TextItem(TELEVISION, R.string.quest_street_cabinet_television),
-        TextItem(GAS, R.string.quest_street_cabinet_gas),
+        TextItem(GAS, R.string.quest_utility_gas),
         TextItem(STREET_LIGHTING, R.string.quest_street_cabinet_street_lighting),
-        TextItem(WATER, R.string.quest_street_cabinet_water),
-        TextItem(SEWERAGE, R.string.quest_street_cabinet_sewerage),
+        TextItem(WATER, R.string.quest_utility_water),
+        TextItem(SEWERAGE, R.string.quest_utility_sewerage),
     )
 }

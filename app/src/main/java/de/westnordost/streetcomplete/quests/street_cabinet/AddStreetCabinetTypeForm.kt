dc38399c249af92546c7d5f6f229627b1dc00389
mcliquid <info@mcliquid.de>
2023-10-31 21:43:10 +0100

    Move text below icons

--- app/src/main/java/de/westnordost/streetcomplete/quests/street_cabinet/AddStreetCabinetTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/street_cabinet/AddStreetCabinetTypeForm.kt
@@ -1,5 +1,7 @@
 package de.westnordost.streetcomplete.quests.street_cabinet
 
+import android.os.Bundle
+import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestForm
 
 class AddStreetCabinetTypeForm : AImageListQuestForm<StreetCabinetType, StreetCabinetType>() {
@@ -7,6 +9,11 @@ class AddStreetCabinetTypeForm : AImageListQuestForm<StreetCabinetType, StreetCa
     override val items = StreetCabinetType.values().map { it.asItem() }
     override val itemsPerRow = 4
     override val moveFavoritesToFront = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        imageSelector.cellLayoutId = R.layout.cell_icon_select_with_label_below
+    }
 
     override fun onClickOk(selectedItems: List<StreetCabinetType>) {
         applyAnswer(selectedItems.single())

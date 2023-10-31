ac169160bc237188c8e8eb3d79c5d9943e6782ec
mcliquid <info@mcliquid.de>
2023-10-31 21:56:01 +0100

    Switched order to most used regarding taginfo

--- app/src/main/java/de/westnordost/streetcomplete/quests/street_cabinet/StreetCabinetType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/street_cabinet/StreetCabinetType.kt
@@ -1,16 +1,16 @@
 package de.westnordost.streetcomplete.quests.street_cabinet
 
 enum class StreetCabinetType(val osmKey: String, val osmValue: String) {
-    POSTAL_SERVICE("street_cabinet", "postal_service"),
+    POWER("utility", "power"),
+    TELECOM("utility", "telecom"),
     TRAFFIC_CONTROL("street_cabinet", "traffic_control"),
-    TRAFFIC_MONITORING("street_cabinet", "traffic_monitoring"),
+    POSTAL_SERVICE("street_cabinet", "postal_service"),
+    GAS("utility", "gas"),
+    STREET_LIGHTING("utility", "street_lighting"),
     TRANSPORT_MANAGEMENT("street_cabinet", "transport_management"),
+    TRAFFIC_MONITORING("street_cabinet", "traffic_monitoring"),
     WASTE("street_cabinet", "waste"),
     TELEVISION("utility", "television"),
-    GAS("utility", "gas"),
-    POWER("utility", "power"),
-    STREET_LIGHTING("utility", "street_lighting"),
-    TELECOM("utility", "telecom"),
     WATER("utility", "water"),
     SEWERAGE("utility", "sewerage");
 }

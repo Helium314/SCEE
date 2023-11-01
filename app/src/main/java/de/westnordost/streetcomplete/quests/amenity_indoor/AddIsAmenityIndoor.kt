644cab437518afb0f025675d5826ac7a06813bde
Helium314 <helium314@mailbox.org>
2023-11-01 10:46:21 +0100

    Revert "exclude building=roof from amenity_indoor quest"
    This reverts commit ca49fcf063960f18ea21aee5b9ef77107b0c719b.

--- app/src/main/java/de/westnordost/streetcomplete/quests/amenity_indoor/AddIsAmenityIndoor.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/amenity_indoor/AddIsAmenityIndoor.kt
@@ -31,11 +31,8 @@ class AddIsAmenityIndoor(private val getFeature: (tags: Map<String, String>) -> 
     """.toElementFilterExpression() }
 
     /* We only want survey nodes within building outlines. */
-    // exclude building=roof, see https://github.com/streetcomplete/StreetComplete/issues/5333
     private val buildingFilter by lazy { """
-        ways, relations with 
-            building
-            and building != roof 
+        ways, relations with building
     """.toElementFilterExpression() }
 
     override val changesetComment = "Determine whether amenities are inside buildings"

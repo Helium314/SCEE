0fa46c1bf25dbd899b1e8360de5c57d80f2611c0
mcliquid <info@mcliquid.de>
2023-11-09 21:42:22 +0100

    try to fix the merge conflict 2

--- app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -589,10 +589,10 @@ fun getQuestTypeList(
     EE_QUEST_OFFSET + 23 to AddRailwayPlatformRef(),
     EE_QUEST_OFFSET + 9 to AddTreeGenus(),
     EE_QUEST_OFFSET + 26 to AddIsPharmacyDispensing(),
-    EE_QUEST_OFFSET + 32 to AddPisteRef(),
     EE_QUEST_OFFSET + 28 to AddFootwayWidth(arSupportChecker),
     EE_QUEST_OFFSET + 10 to OsmoseQuest(osmoseDao),
     EE_QUEST_OFFSET + 11 to CustomQuest(customQuestList),
+    EE_QUEST_OFFSET + 32 to AddPisteRef(),
     // POI quests
     EE_QUEST_OFFSET + 12 to ShowBusiness(),
     EE_QUEST_OFFSET + 13 to ShowBicycleStuff(),

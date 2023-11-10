dc480cdd7c14b444865f826dcadc1cef8761bf00
mcliquid <info@mcliquid.de>
2023-11-10 15:02:24 +0100

    Fixed binding

--- app/src/main/java/de/westnordost/streetcomplete/quests/piste_ref/AddPisteRefForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/piste_ref/AddPisteRefForm.kt
@@ -2,23 +2,22 @@ package de.westnordost.streetcomplete.quests.piste_ref
 
 import androidx.appcompat.app.AlertDialog
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.databinding.QuestRefBinding
+import de.westnordost.streetcomplete.databinding.ViewPisteRefBinding
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.quests.AnswerItem
 import de.westnordost.streetcomplete.util.ktx.nonBlankTextOrNull
 
 class AddPisteRefForm : AbstractOsmQuestForm<PisteRefAnswer>() {
 
-    //override val contentLayoutResId = R.layout.view_piste_ref
-    override val contentLayoutResId = R.layout.quest_ref
-    private val binding by contentViewBinding(QuestRefBinding::bind)
+    override val contentLayoutResId = R.layout.view_piste_ref
+    private val binding by contentViewBinding(ViewPisteRefBinding::bind)
 
     override val otherAnswers get() =
         listOfNotNull(
             AnswerItem(R.string.quest_piste_ref_connection) { confirmPisteConnection() }
         )
 
-    private val ref get() = binding.refInput.nonBlankTextOrNull
+    private val ref get() = binding.pisteRefInput.nonBlankTextOrNull
 
     override fun onClickOk() {
         applyAnswer(PisteRef(ref!!))

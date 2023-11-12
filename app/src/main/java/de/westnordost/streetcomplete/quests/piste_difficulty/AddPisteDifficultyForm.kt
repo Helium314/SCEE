23846a8a9cf0addc335c1ca84ac18efbfffa886d
mcliquid <info@mcliquid.de>
2023-11-12 12:25:24 +0100

    tried to add region specific signs

--- app/src/main/java/de/westnordost/streetcomplete/quests/piste_difficulty/AddPisteDifficultyForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/piste_difficulty/AddPisteDifficultyForm.kt
@@ -1,10 +1,12 @@
 package de.westnordost.streetcomplete.quests.piste_difficulty
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestForm
 
-class AddPisteDifficultyForm : AImageListQuestForm<PisteDifficulty, List<PisteDifficulty>>() {
+class AddPisteDifficultyForm : AImageListQuestForm<PisteDifficulty, PisteDifficulty>() {
 
     override val items = PisteDifficulty.values().map { it.asItem() }
     override val itemsPerRow = 2
@@ -15,7 +17,20 @@ class AddPisteDifficultyForm : AImageListQuestForm<PisteDifficulty, List<PisteDi
         imageSelector.cellLayoutId = R.layout.cell_labeled_icon_select_with_description
     }
 
-    override fun onClickOk(selectedItems: List<PisteDifficulty>) {
-        applyAnswer(selectedItems)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageView>(R.id.imageView)
+            .setImageResource(getRegionalPisteDifficultyResId(countryInfo.countryCode))
     }
+
+    override fun onClickOk(selectedItems: List<PisteDifficulty>) {
+        applyAnswer(selectedItems.single())
+    }
+}
+
+private fun getRegionalPisteDifficultyResId(countryCode: String): Int = when (countryCode) {
+    "AT" -> R.drawable.ic_quest_piste_difficulty_novice
+    "JP" -> R.drawable.ic_quest_piste_difficulty_easy
+    else -> R.drawable.ic_quest_piste_difficulty_intermediate
 }

84b1a04bdc9e101b5c3d9e4664ccba939a412743
Wieland <29458682+wielandb@users.noreply.github.com>
2024-01-02 18:53:42 +0100

    Make `AddParkingOrientation` a AListQuestForm

--- app/src/main/java/de/westnordost/streetcomplete/quests/parking_orientation/AddParkingOrientationForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/parking_orientation/AddParkingOrientationForm.kt
@@ -1,13 +1,18 @@
 package de.westnordost.streetcomplete.quests.parking_orientation
 
-import de.westnordost.streetcomplete.quests.AImageListQuestForm
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.quests.AListQuestForm
+import de.westnordost.streetcomplete.quests.TextItem
+import de.westnordost.streetcomplete.quests.parking_orientation.ParkingOrientation.DIAGONAL
+import de.westnordost.streetcomplete.quests.parking_orientation.ParkingOrientation.PARALLEL
+import de.westnordost.streetcomplete.quests.parking_orientation.ParkingOrientation.PERPENDICULAR
 
-class AddParkingOrientationForm : AImageListQuestForm<ParkingOrientation, ParkingOrientation>() {
 
-    override val items = ParkingOrientation.values().map { it.asItem() }
-    override val itemsPerRow = 3
+class AddParkingOrientationForm : AListQuestForm<ParkingOrientation>() {
 
-    override fun onClickOk(selectedItems: List<ParkingOrientation>) {
-        applyAnswer(selectedItems.single())
-    }
+    override val items = listOf(
+        TextItem(PARALLEL, R.string.quest_parking_orientation_answer_parallel),
+        TextItem(DIAGONAL, R.string.quest_parking_orientation_answer_diagonal),
+        TextItem(PERPENDICULAR, R.string.quest_parking_orientation_answer_perpendicular),
+    )
 }

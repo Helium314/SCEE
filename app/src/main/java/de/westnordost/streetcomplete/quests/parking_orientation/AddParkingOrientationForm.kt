5581b2ac541b04910bc22829873c506ee0f300d1
Wieland <29458682+wielandb@users.noreply.github.com>
2024-03-26 00:08:25 +0100

    Remove unneeded imports

--- app/src/main/java/de/westnordost/streetcomplete/quests/parking_orientation/AddParkingOrientationForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/parking_orientation/AddParkingOrientationForm.kt
@@ -1,11 +1,5 @@
 package de.westnordost.streetcomplete.quests.parking_orientation
 
-import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.quests.AListQuestForm
-import de.westnordost.streetcomplete.quests.TextItem
-import de.westnordost.streetcomplete.quests.parking_orientation.ParkingOrientation.DIAGONAL
-import de.westnordost.streetcomplete.quests.parking_orientation.ParkingOrientation.PARALLEL
-import de.westnordost.streetcomplete.quests.parking_orientation.ParkingOrientation.PERPENDICULAR
 import de.westnordost.streetcomplete.quests.AImageListQuestForm
 
 class AddParkingOrientationForm : AImageListQuestForm<ParkingOrientation, ParkingOrientation>() {

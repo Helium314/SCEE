a10d5644300c43a35d280dd900ebc4ef20008beb
Helium314 <helium314@mailbox.org>
2024-03-27 16:55:30 +0100

    use the actually correct binding...

--- app/src/main/java/de/westnordost/streetcomplete/quests/parking_capacity/AddParkingCapacityForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/parking_capacity/AddParkingCapacityForm.kt
@@ -2,17 +2,16 @@ package de.westnordost.streetcomplete.quests.parking_capacity
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isGone
 import androidx.core.widget.doAfterTextChanged
 import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.databinding.QuestDisabledParkingCapacityBinding
+import de.westnordost.streetcomplete.databinding.QuestCarParkingCapacityBinding
 import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
 import de.westnordost.streetcomplete.util.ktx.intOrNull
 
 class AddParkingCapacityForm : AbstractOsmQuestForm<Int>() {
 
     override val contentLayoutResId = R.layout.quest_car_parking_capacity
-    private val binding by contentViewBinding(QuestDisabledParkingCapacityBinding::bind)
+    private val binding by contentViewBinding(QuestCarParkingCapacityBinding::bind)
 
     private val capacity get() = binding.capacityInput.intOrNull ?: 0
 

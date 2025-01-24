5d02b0217be416a4fc8a5467c1b9d6be34be5352
Flo Edelmann <git@flo-edelmann.de>
2025-01-24 22:02:12 +0100

    Add roof orientation quest

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/roof_orientation/AddRoofOrientation.kt
@@ -0,0 +1,38 @@
+package de.westnordost.streetcomplete.quests.roof_orientation
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.BUILDING
+import de.westnordost.streetcomplete.osm.Tags
+
+class AddRoofOrientation : OsmFilterQuestType<String>() {
+
+    override val elementFilter = """
+        ways with
+          roof:shape = gabled
+          and !roof:orientation
+          and !roof:direction
+          and building !~ no|construction
+          and location != underground
+          and ruins != yes
+    """
+    override val changesetComment = "Add roof orientation"
+    override val wikiLink = "Key:roof:orientation"
+    override val icon = R.drawable.ic_quest_roof_orientation
+    override val achievements = listOf(BUILDING)
+    override val defaultDisabledMessage = R.string.default_disabled_msg_roofColour
+
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_roofOrientation_title
+
+    override fun createForm() = AddRoofOrientationForm()
+
+    override fun applyAnswerTo(
+        answer: String,
+        tags: Tags,
+        geometry: ElementGeometry,
+        timestampEdited: Long,
+    ) {
+        tags["roof:orientation"] = answer
+    }
+}

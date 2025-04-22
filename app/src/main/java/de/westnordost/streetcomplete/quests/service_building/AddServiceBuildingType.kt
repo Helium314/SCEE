adf60266da2f258e982cdfcce9e8c8e8ed82b574
Matija Nalis <mnalis-git@voyager.hr>
2025-04-22 05:37:50 +0200

    remove whitespace

--- app/src/main/java/de/westnordost/streetcomplete/quests/service_building/AddServiceBuildingType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/service_building/AddServiceBuildingType.kt
@@ -22,7 +22,6 @@ class AddServiceBuildingType : OsmFilterQuestType<ServiceBuildingType>() {
     override val changesetComment = "Add service building type"
     override val wikiLink = "Tag:building=service"
     override val icon = R.drawable.ic_quest_service_building
-
     override val defaultDisabledMessage: Int = R.string.default_disabled_msg_ee
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_service_building_type_title

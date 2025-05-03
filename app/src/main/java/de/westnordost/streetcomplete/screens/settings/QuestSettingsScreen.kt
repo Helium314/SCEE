ede8c276e17a9c4e6ed7e85ad1b72ada63c9173a
Matija Nalis <mnalis-git@voyager.hr>
2025-05-03 20:55:24 +0200

    try to force overriden quests to show up automatically without manually downloading quests

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/QuestSettingsScreen.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/QuestSettingsScreen.kt
@@ -117,7 +117,7 @@ fun QuestSettingsScreen(
                     description = stringResource(R.string.pref_override_country_restrictions_summary),
                     pref = Prefs.OVERRIDE_COUNTRY_RESTRICTIONS,
                     default = false,
-                    onCheckedChange = { OsmQuestController.reloadQuestTypes() },
+                    onCheckedChange = { OsmQuestController.reloadQuestTypes(); scope.launch(Dispatchers.IO) { visibleEditTypeController.onVisibilitiesChanged() } },
                 )
             Preference(
                 name = stringResource(R.string.pref_quest_monitor_title),

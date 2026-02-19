6c043cf994732512ab03a48b4a983972e1abab57
dokutan <54861821+dokutan@users.noreply.github.com>
2026-02-19 23:35:43 +0100

    Move PavingStonesMaterial to Compose

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/paving_stones_material/AddPavingStonesMaterialForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/paving_stones_material/AddPavingStonesMaterialForm.kt
@@ -1,22 +1,21 @@
 package de.westnordost.streetcomplete.quests.paving_stones_material
 
-import de.westnordost.streetcomplete.quests.AItemSelectQuestForm
-import org.jetbrains.compose.resources.painterResource
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
+import de.westnordost.streetcomplete.quests.AItemSelectQuestForm
 import de.westnordost.streetcomplete.ui.common.item_select.ImageWithLabel
 import kotlinx.serialization.serializer
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 class AddPavingStonesMaterialForm : AItemSelectQuestForm<PavingStonesMaterial, PavingStonesMaterial>() {
 
     override val items = PavingStonesMaterial.entries
+    override val itemsPerRow = 3
     override val serializer = serializer<PavingStonesMaterial>()
 
     @Composable override fun ItemContent(item: PavingStonesMaterial) {
-        ImageWithLabel(painterResource(item.imageResId), stringResource(item.titleResId))
+        ImageWithLabel(painterResource(item.icon), stringResource(item.title))
     }
-
-    override val itemsPerRow = 3
 
     override fun onClickOk(selectedItem: PavingStonesMaterial) {
         applyAnswer(selectedItem)

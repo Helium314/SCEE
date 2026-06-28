4d090d04645113e10ac0c29ef5d612c3c1129d56
Helium314 <helium314@mailbox.org>
2026-06-28 16:32:57 +0200

    compose for a few text input quests

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/guidepost/AddGuidepostEleForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/guidepost/AddGuidepostEleForm.kt
@@ -1,29 +1,14 @@
 package de.westnordost.streetcomplete.quests.guidepost
 
-import android.os.Bundle
-import android.view.View
-import androidx.core.widget.doAfterTextChanged
-import de.westnordost.streetcomplete.R
-import de.westnordost.streetcomplete.databinding.QuestGuidepostEleBinding
-import de.westnordost.streetcomplete.quests.AbstractOsmQuestForm
-import de.westnordost.streetcomplete.util.ktx.nonBlankTextOrNull
+import androidx.compose.ui.text.input.KeyboardType
+import de.westnordost.streetcomplete.quests.TextInputForm
 
-class AddGuidepostEleForm : AbstractOsmQuestForm<String>() {
-
-    override val contentLayoutResId = R.layout.quest_guidepost_ele
-    private val binding by contentViewBinding(QuestGuidepostEleBinding::bind)
-
-    private val ele get() = binding.refInput.nonBlankTextOrNull
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.refInput.doAfterTextChanged { checkIsFormComplete() }
-    }
+class AddGuidepostEleForm : TextInputForm<String>() {
+    override val keyboardType = KeyboardType.Decimal
 
     override fun onClickOk() {
-        applyAnswer(ele!!)
+        applyAnswer(text.value)
     }
 
-
-    override fun isFormComplete() = ele != null
+    override fun isFormComplete() = text.value.toDoubleOrNull() != null
 }

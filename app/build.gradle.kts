700513d5e0b75b9916d0e4b4caa566500f343dc6
Helium314 <helium314@mailbox.org>
2026-07-03 15:26:34 +0200

    upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ import java.util.Properties
 
 
 /** App version name, code and flavor */
-val appVersionName = "63.2"
-val appVersionCode = 6305
+val appVersionName = "63.21"
+val appVersionCode = 6306
 
 /** Localizations the app should be available in */
 val bcp47ExportLanguages = setOf(
